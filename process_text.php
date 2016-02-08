adf23074f34d4c8f659b03cbb367efeb002fe4ee
Alex Quang <alexquang1991@gmail.com>
2016-02-07 21:35:51 -0800

    Added the output page and added a process page to process text

--- process_text.php
+++ process_text.php
@@ -2,21 +2,47 @@
 	session_start();
 	require_once("new_connection.php");
 
-	if(isset($_GET['action']) && $_GET['action'] == 'messaging')
+	if(isset($_POST['action']) && $_POST['action'] == 'messaging')
 	{
-		post_message($_GET);
+		insert_message($_POST);
 	}
-	if(isset($_GET['action']) && $_GET['action'] == 'comments')
+	if(isset($_POST['action']) && $_POST['action'] == 'comments')
 	{
-		post_comment($_GET);
+		insert_comment($_POST);
 	}
 
-	function post_message($get) // This is just a parameter named $get, not the actual variable $_GET
+	function insert_message($post) // This is just a parameter named $post, not the actual variable $_POST
 	{
 		// Making sure that the message is not blank, not gonna allow people to post blank messages. That would be annoying.
-		if(empty($get['message']))
+		if(trim($post['message']) == "")
 		{
+			$_SESSION['blank'] = "Your message cannot be blank!";
+			header('Location: wall.php');
+			exit();
+		}
+		if(!trim($post['message']) == "")
+		{
+			$query = "INSERT INTO messages (user_id, message, created_at, updated_at) VALUES ('{$_SESSION['user_id']}', '{$post['message']}', NOW(), NOW())";
+			run_mysql_query($query);
+			header('Location: wall.php');
+			exit();
+		}
+	}
 
+	function insert_comment($post)
+	{
+		if(empty($post['comment']))
+		{
+			$_SESSION['blank'] = "Your comment cannot be blank!";
+			header('Location: wall.php');
+			exit();
+		}
+		if(!empty($post['comment']))
+		{
+			$query = "INSERT INTO comments (message_id, user_id, comment, created_at, updated_at) VALUES ('{$_SESSION['message_id']}', '{$_SESSION['user_id']}', '{$post['comment']}', NOW(), NOW())";
+			run_mysql_query($query);
+			header('Location: wall.php');
+			exit();
 		}
 	}
 ?>
--- wall.php
+++ wall.php
@@ -1,6 +1,19 @@
 <?php
 	session_start();
 	require_once("new_connection.php");
+	$join_tables_query = "SELECT users.first_name, users.last_name, messages.message, messages.created_at, messages.message, messages.id as message_id
+					  	  FROM users
+						  LEFT JOIN messages ON users.id = messages.user_id
+						  WHERE users.id = {$_SESSION['user_id']}";
+	$messages = fetch_all($join_tables_query);
+
+	$comments_query = "SELECT users.first_name, users.last_name, comments.comment, comments.created_at, comments.message_id as message_id
+				       FROM users
+					   JOIN messages ON users.id = messages.user_id
+					   JOIN comments on messages.id = comments.message_id
+					   WHERE users.id = {$_SESSION['user_id']}";
+	$comments = fetch_all($comments_query);
+	$_SESSION['message_id'] = $comments[0]['message_id'];
 
 ?>
 
@@ -22,14 +35,50 @@
 		<div id="main-content">
 			<div class="post-box">
 				<h3>Post a message</h3>
-				<form class="message-form" action="process_text.php" method="get">
+				<form class="message-form" action="process_text.php" method="post">
+					<input type="hidden" name="action" value="messaging">
 					<textarea class="text-input" name="message" rows="4"></textarea>
-					<input type="hidden" value="action" value="messaging">
 					<p class="post-btn"><input class="message-btn" type="submit" value="Post a message"></p>
 				</form>
+				<?php 
+					if(isset($_SESSION['blank'])) 
+					{ 
+						echo "<p class=blank>{$_SESSION['blank']}</p>"; 
+						unset($_SESSION['blank']); 
+					} 
+				?>
 			</div>
 			<div class="messages-box">
-				
+				<?php 
+					if(isset($messages) && !empty($messages))
+					{ ?>
+						<ol>
+				<?php	foreach($messages as $message)
+						{ ?>
+							<li><p class="content-m"><?php echo $message['message']; ?></p>
+							<small>by <?php echo $message['first_name']; ?> | <?php echo $message['created_at']; ?></small> 
+						<ul>					
+				<?php	foreach($comments as $comment)
+						{ 
+							if($comment['message_id'] == $message['message_id'])
+							{ ?>
+							<li><p class="content-c"><?php echo $comment['comment']; ?></p>
+							<small>by <?php echo $comment['first_name'] ?> | <?php echo $comment['created_at']; ?></small>
+							</li>
+				<?php 		}
+						} ?>
+							<li>
+								<form action="process_text.php" method="post">
+									<input type="hidden" name="action" value="comments">
+									<input type="hidden" name="message_id" value='<?= $message['message_id'] ?>'>
+									<input type="text" name="comment">
+									<button class="comment-btn" type="submit">Comment</button>
+								</form>
+							</li>
+						</ul>
+				<?php   } ?>
+						</ol>
+		<?php	  } ?>
 			</div>
 		</div>
 	</div>
